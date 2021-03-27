3150c6b9d2b2e78b7c45f6f0d81183101ae56337
Gera <alxgeras@mail.ru>
2021-03-27 19:08:17 +0300

    исправление замечания

--- задание 2/task_2.sql
+++ задание 2/task_2.sql
@@ -31,48 +31,25 @@ CREATE TABLE posts (
   CONSTRAINT fk_posts_media_id FOREIGN KEY (media_id) REFERENCES media(id)
 );
 
-
-
-/* стркутрура таблицы типов лайков(для таблицы лайков)
- * id  - id типа
- * name - тип сущности которой мы хотим поставить лайк (сообщение, пользователь, медиафайл, пост)
- * */
-
-DROP TABLE IF EXISTS target_types;
-
-CREATE TABLE target_types (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL UNIQUE
-);
-
-INSERT INTO target_types (name) VALUES 
-  ('messages'),
-  ('users'),
-  ('media'),
-  ('posts');
  
- /* стркутрура лайков
+ /* стркутрура лайков постов
  * id - id лайка
  * user_id - id пользователя поставившего лайк
- * target_id - id сущности которой ставится лайк
- * target_type_id - id типа сущности которой ставится лайк 
+ * post_id - id поста которму ставится лайк
  * created_at - дата лайка
  * */
 
 DROP TABLE IF EXISTS likes;
 
 CREATE TABLE likes (
-  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   user_id BIGINT UNSIGNED NOT NULL,
-  target_id BIGINT UNSIGNED NOT NULL,
-  target_type_id INT UNSIGNED NOT NULL,
+  post_id BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY(user_id, post_id),
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   INDEX likes_user_idx (user_id),
-  CONSTRAINT fk_likes_target_id_1 FOREIGN KEY (target_id) REFERENCES messages(id),
-  CONSTRAINT fk_likes_target_id_2 FOREIGN KEY (target_id) REFERENCES users(id),
-  CONSTRAINT fk_likes_target_id_3 FOREIGN KEY (target_id) REFERENCES media(id),
-  CONSTRAINT fk_likes_target_id_4 FOREIGN KEY (target_id) REFERENCES posts(id),
-  CONSTRAINT fk_likes_target_type_id FOREIGN KEY (target_type_id) REFERENCES target_types(id)
+  INDEX likes_post_idx (post_id),
+  CONSTRAINT fk_likes_post_id FOREIGN KEY (post_id) REFERENCES posts(id),
+  CONSTRAINT fk_likes_user_id FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 
